052625dc6094c9efbfa41565ec8a8a337285622c
Jesass351 <mikanev03@mail.ru>
2022-03-09 18:16:57 +0300

    Server 1.0 Artems Igor

---
+++ mytcpserver.cpp
@@ -0,0 +1,49 @@
+#include "mytcpserver.h"
+#include <QDebug>
+#include <QCoreApplication>
+#include <iostream>
+
+MyTcpServer::~MyTcpServer()
+{
+    mTcpServer->close();
+    //server_status=0;
+}
+MyTcpServer::MyTcpServer(QObject *parent) : QObject(parent){
+    mTcpServer = new QTcpServer(this);
+
+    connect(mTcpServer, &QTcpServer::newConnection,
+            this, &MyTcpServer::slotNewConnection);
+    if(!mTcpServer->listen(QHostAddress::Any, 33333)){
+        qDebug() << "server is not started";
+    } else {
+        //server_status=1;
+        qDebug() << "server is started";
+    }
+}
+
+void MyTcpServer::slotNewConnection(){
+ //   if(server_status==1){
+        QTcpSocket *tempSocket;
+        tempSocket = mTcpServer->nextPendingConnection();
+        mTcpSocket.push_back(tempSocket);
+        tempSocket->write("Hello, World!!! I am echo server!\r\n");
+        connect(tempSocket, &QTcpSocket::readyRead,this,&MyTcpServer::slotServerRead);
+        connect(tempSocket,&QTcpSocket::disconnected,this, &MyTcpServer::slotServerRead);
+   // }
+}
+
+void MyTcpServer::slotServerRead(){
+    QTcpSocket *tempSock = (QTcpSocket*)sender();
+    while(tempSock->bytesAvailable()>0){
+                QString str = tempSock->readAll();
+                QByteArray array;
+                array.append(str);
+                tempSock->write(array);}
+}
+
+
+void MyTcpServer::slotClientDisconnected(){
+        QTcpSocket *tempSock = (QTcpSocket*)sender();
+        mTcpSocket.remove(tempSock);
+        tempSock->close();
+}
